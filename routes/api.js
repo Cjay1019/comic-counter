237319bcf4994477f9ebd91729e0c5daf615ace4
Conner Leigh <cjay741@gmail.com>
2024-02-07 02:06:33 -0500

    adding logs

--- routes/api.js
+++ routes/api.js
@@ -4,6 +4,7 @@ module.exports = function (app) {
     app.post("/api/getReadCount", (req, res) => {
         lofcg.readList.get(req.body.userId, { type: lofcg.types.ISSUE }, function (err, results) {
             if (err) console.error(err)
+            console.log(results)
             res.json({ readCount: results.length - req.body.startCount });
         });
     });
