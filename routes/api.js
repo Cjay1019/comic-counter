866233652c9dce6f1d5b9db2225a051044ce24c9
Conner Leigh <cjay741@gmail.com>
2024-02-07 02:20:19 -0500

    adding logs

--- routes/api.js
+++ routes/api.js
@@ -2,6 +2,7 @@ const lofcg = require("../leagueofcomicgeeks/index");
 
 module.exports = function (app) {
     app.post("/api/getReadCount", (req, res) => {
+        console.log(req.body)
         lofcg.readList.get(req.body.userId, { type: lofcg.types.ISSUE }, function (err, results) {
             if (err) console.error(err)
             console.log(results)
