9094c2f1dfe7a227f873f557c8b56bafaa9dc901
Conner Leigh <cjay741@gmail.com>
2024-02-09 11:09:45 -0500

    remove log

--- routes/api.js
+++ routes/api.js
@@ -5,7 +5,6 @@ module.exports = function (app) {
         console.log(req.body)
         lofcg.readList.get(req.body.userId, { type: lofcg.types.ISSUE }, function (err, results) {
             if (err) console.error(err)
-            console.log(results)
             res.json({ readCount: results.length - req.body.startCount });
         });
     });
