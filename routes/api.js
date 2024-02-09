b6328162daea07dedf47cfa44d6d479393ea4fcb
Conner Leigh <cjay741@gmail.com>
2024-02-09 13:35:52 -0500

    error handling

--- routes/api.js
+++ routes/api.js
@@ -4,7 +4,10 @@ module.exports = function (app) {
     app.post("/api/getReadCount", (req, res) => {
         console.log(req.body)
         lofcg.readList.get(req.body.userId, { type: lofcg.types.ISSUE }, function (err, results) {
-            if (err) console.error(err)
+            if (err) console.error(err);
+            if (!results) {
+                results = []
+            }
             res.json({ readCount: results.length - req.body.startCount });
         });
     });
