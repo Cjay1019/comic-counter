1312e26fd7e0d51ed9cacd94b7dc235a550ab1a7
Conner Leigh <cjay741@gmail.com>
2024-02-09 20:54:09 -0500

    user agent test

--- leagueofcomicgeeks/src/utils/request.js
+++ leagueofcomicgeeks/src/utils/request.js
@@ -4,5 +4,6 @@ const config = require("../../config");
 
 module.exports = request.defaults({
   jar: authentication.cookieJar,
-  baseUrl: config.rootUrl
+  baseUrl: config.rootUrl,
+  headers: {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36"} 
 });
