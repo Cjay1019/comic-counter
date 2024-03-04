a220d67f08cdfbc571b9a6e489eafe5bca47abd6
Conner Leigh <cjay741@gmail.com>
2024-03-04 10:08:01 -0500

    added missing json parsing

--- lambdas/getReadCount.mjs
+++ lambdas/getReadCount.mjs
@@ -2,9 +2,10 @@ import * as lofcg from "./leagueofcomicgeeks/index.js";
 
 export async function handler(event, context) {
     console.log(event.body);
+    const data = JSON.parse(event.body);
 
     const response = await new Promise((resolve) => {
-        lofcg.readList.get(event.body.userId, { type: lofcg.types.ISSUE }, function (err, results) {
+        lofcg.readList.get(data.userId, { type: lofcg.types.ISSUE }, function (err, results) {
             if (err) {
                 console.error(err);
                 resolve(0);
@@ -13,8 +14,8 @@ export async function handler(event, context) {
             if (!results) {
                 results = []
             }
-
-            resolve({ readCount: results.length - event.body.startCount});
+            
+            resolve({ readCount: results.length - data.startCount});
         });
     });
 
