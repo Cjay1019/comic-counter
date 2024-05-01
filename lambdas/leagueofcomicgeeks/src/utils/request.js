54651e92de707fc2ce91413fbb8ce33376390745
Conner Leigh <cjay741@gmail.com>
2024-05-01 10:31:04 -0400

    change to how proxy creds work, new env variable

--- lambdas/leagueofcomicgeeks/src/utils/request.js
+++ lambdas/leagueofcomicgeeks/src/utils/request.js
@@ -5,5 +5,8 @@ const config = require("../../config");
 module.exports = request.defaults({
   jar: authentication.cookieJar,
   baseUrl: config.rootUrl,
-  headers: {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36"} 
+  headers: {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36",
+    "Proxy-Authorization": `Basic ${Buffer.from(process.env.CREDS).toString("base64")}`
+} 
 });
